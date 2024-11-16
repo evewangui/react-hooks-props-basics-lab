292e16cfca42b8cccd9ec0fbab386782a6f6ffb2
eve <evewangui447@gmail.com>
2024-11-16 07:49:33 +0300

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+
+import React from "react";
+import user from "../data/user";
+import Links from "./Links";
+
+
+function About() {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>{user.bio}</p>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      
+     <Links/>
+
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import user from "../data/user";
+
+
+function Home() {
+  return (
+    <div id="home">
+      <h1 style={{ color: user.color }}>
+        {user.name} is a Web Developer from {user.city}
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
